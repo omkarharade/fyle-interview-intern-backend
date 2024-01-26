ba1ea51ead4b742068de7575596917c494906d3f
Omkar Harade <omkarharade1301@gmail.com>
2024-01-26 15:45:16 +0530

    added dockerfile and compose file

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@
 set -e
 
 # Delete older .pyc files
-# find . -type d \( -name env -o -name venv  \) -prune -false -o -name "*.pyc" -exec rm -rf {} \;
+find . -type d \( -name env -o -name venv  \) -prune -false -o -name "*.pyc" -exec rm -rf {} \;
 
 # Run required migrations
 export FLASK_APP=core/server.py
 
-# flask db init -d core/migrations/
-# flask db migrate -m "Initial migration." -d core/migrations/
-# flask db upgrade -d core/migrations/
+flask db init -d core/migrations/
+flask db migrate -m "Initial migration." -d core/migrations/
+flask db upgrade -d core/migrations/
 
 # Run server
-gunicorn -c gunicorn_config.py core.server:app
+gunicorn -c ./gunicorn_config.py core.server:app
